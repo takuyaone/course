2e88fce154fc82c65f0e94ce9a11fee937567006
takuyaone <one.amaza@gmail.com>
2022-02-21 09:35:58 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+
+$('.question-title').on('click', function () {//タイトル要素をクリックしたら
+    var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+    $(findElm).slideToggle();//アコーディオンの上下動作
+
+    if ($(this).hasClass('close')) {//タイトル要素にクラス名closeがあれば
+        $(this).removeClass('close');//クラス名を除去し
+    } else {//それ以外は
+        $(this).addClass('close');//クラス名closeを付与
+    }
+});
+
